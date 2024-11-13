b988632f0899fca8233c80098984375c3992f542
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-11-12 19:46:55 -0600

    Resolve Function and test, included the test in the graphql2 file.

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,29 +1,47 @@
 // Schema.js
 // This file is the schema for the GraphQL API.
 // BJC 2024-10-28
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
+const { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLInt } = require("graphql");
+const _ = require('lodash');
+
+const tasks = [
+  {
+    id: '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+  },
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+  }
+];
 
 const TaskType = new GraphQLObjectType({
-    name: 'Task',
-    fields: () => ({
-        id: { type: GraphQLString },
-        title: { type: GraphQLString },
-        weight: { type: GraphQLInt },
-        description: { type: GraphQLString }
-    })
-})
+  name: 'Task',
+  fields: () => ({
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      task: {
-        type: TaskType,
-        args: { id: { type: GraphQLString } },
-        resolve(parent, args) {}
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return _.find(tasks, { id: args.id });
       }
     }
-  });
-  
-  module.exports = new GraphQLSchema({
-    query: RootQuery
-  });
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
