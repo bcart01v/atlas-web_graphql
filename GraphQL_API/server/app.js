a669cb4cf36764c377bab6f7f668ce610fb18b37
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2024-11-12 21:59:12 -0600

    Connecting to mongoDB Atlas and create mongoose models

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+const mongoURI = 'mongodb+srv://bcart0v:AuOzQQg9Qg1JDTl5@bendev.nwbbc.mongodb.net/?retryWrites=true&w=majority&appName=BenDev';
+
+mongoose.connect(mongoURI);
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
+
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
+  schema,
   graphiql: true
 }));
 
 app.listen(4000, () => {
-  console.log('now listening for request on port 4000');
+  console.log('Listening for requests on port 4000');
 });
---
+++ GraphQL_API/server/models/project.js
@@ -0,0 +1,12 @@
+//server/models/project.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+  title: { type: String, required: true },
+  weight: { type: Number, required: true },
+  description: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Project', projectSchema);
